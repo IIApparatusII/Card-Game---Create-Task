46cdbbb6e585d6b1cca33eb3b5f1f90a062e10af
jonahleibowitz <34136455+jonahleibowitz@users.noreply.github.com>
2019-02-25 13:13:06 -0500

    working

--- gameCode.js
+++ gameCode.js
@@ -1,6 +1,6 @@
 function game(){
   let i
-var[] opponent;
-  var fruits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  console.log fruits[i];
+  var fruits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(fruits.length);
 }
