4b1a113c2b09f8598c5723cbaa43326e0b361d8f
jonahleibowitz <34136455+jonahleibowitz@users.noreply.github.com>
2019-02-27 13:04:34 -0500

    updating js

--- gameCode.js
+++ gameCode.js
@@ -1,6 +1,53 @@
-function game(){
-  let i
-  var fruits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(fruits.length);
+function game(){
+
+  var cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var used= [];
+  let i= cards.length;
+  j=0;
+shuffle();
+//  while (i--) {
+//    j = Math.floor(Math.random() * (i+1));
+//    used.push(cards[j]);
+//    cards.splice(j,1);
+//console.log(cards.length);
+}
+
+
+
+function shuffle([cards]){
+console.log("hello world");
+console.log(cards[1]);
+
+}
+
+function one{
+
+}
+function two{
+
+}
+function three{
+
+}
+function four{
+
+}
+function five{
+
+}
+function six{
+
+}
+function seven{
+
+}
+function eight{
+
+}
+function nine{
+
+}
+function ten{
+
 }
