a6a611436a24ae78a0dae504fd144aed04476fb9
jonahleibowitz <34136455+jonahleibowitz@users.noreply.github.com>
2019-03-05 12:05:04 -0500

    pushing js from home

--- gameCode.js
+++ gameCode.js
@@ -3,38 +3,44 @@ var theirs;
 var yourScore=0;
 var theirScore=0;
 var rounds=0;
+var shuffled;
+var shuffledCards=[];
+
+//Order of Events
+//1. Gives instructions
+//2. Pick first card, assigned to yours (log yours) DONE
+//3.Opponent cards are shuffled (log cards, shuffled cards) DONE
+//4.New shuffled array is saved (log new array to check) DONE
+//5.First value in array is chosen (log first value), and then assigned to theirs (log theirs)DONE
+//6.First value is removed (log shuffledCards after shift)DONE
+//7.Evaluate whether theirs or yours is larger, change score accordingly DONE
+//8.Variable shuffled is set to true (log value of shuffled)DONE
+//9.When choosing second card, program skips shuffle and goes straight to round (shuffledCards should now have 8 values)
+
+//function choose(){
+//  console.log(shuffled)
+//  if(shuffled=0){
+//    game();
+//  }
+//  else{
+//    round(shuffledCards);
+//  }
+//}
+////////FIRST ROUND///////
+
 
 function game(){
   var cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  console.log(cards)
+  console.log(shuffled)
+
+
+//  if(shuffled=false){
   shuffle(cards);
+//  }
+//  else if(shuffled=true){
+//    round(shuffledCards);
+//  }
 }
-
-function round(){
-  theirs=shuffledCards[0];
-  shuffledCards.shift();
-  score();
-}
-function score(){
-if(yours>theirs){
-  yourScore++;
-  console.log(yours)
-  document.getElementById("score").innerHTML = yourScore;
-  }
-  else if(yours<theirs){
-    theirScore++;
-    document.getElementById("score1").innerHTML = theirScore;
-  }
-  else if(yours=theirs){
-    yourScore;
-    theirScore;
-  }
-if(rounds>9){
-    results();
-  }
-}
-
-
 
 function shuffle(cards) {
   var m = cards.length, t, i;
@@ -44,15 +50,47 @@ function shuffle(cards) {
     cards[m] = cards[i];
     cards[i] = t;
   }
-  console.log(cards)
-  var shuffledCards= cards;
-  console.log(shuffledCards)
-  return shuffledCards;
+  //shuffled=1;
+  //console.log(cards)
+   shuffledCards= cards;
+//  console.log(shuffledCards)
+  round(shuffledCards);
 }
+////////////////////////////
 
+////////EVERY ROUND///////
+function round(shuffledCards){
+  theirs=(shuffledCards[0]);
+  console.log("theirs:" + theirs);
+  shuffledCards.shift();
+  console.log(shuffledCards)
+  score();
 
+}
+function score(){
+if(yours>theirs){
+  yourScore++;
+  console.log("yours:" + yours)
+  console.log(shuffled)
+  document.getElementById("score").innerHTML = yourScore;
+  }
+  else if(yours<theirs){
+    theirScore++;
+    console.log("yours:" + yours)
+    console.log(shuffled)
+    document.getElementById("score1").innerHTML = theirScore;
+  }
+  else if(yours=theirs){
+    console.log("yours:" + yours)
+    console.log(shuffled)
+    yourScore;
+    theirScore;
+  }
 
-
+if(rounds>9){
+    results();
+  }
+}
 
 function results(){
   if(yourScore>theirScore){
@@ -65,75 +103,79 @@ function results(){
     alert("Tie.")
   }
 }
+////////////////////////////////
 
 
-
-
+///ANIMATION/////
 function showCard(){
 var image=document.getElementById(img2);
 image.src='twoofhearts.jpg';
-
 }
+/////////
 
+/// YOUR CARD FUNCTIONS///
 function one(){
 yours=1;
 rounds++;
 document.getElementById("One").disabled = true;
-round();
+console.log(yours)
+game();
 }
+
 function two(){
 yours=2;
 rounds++;
 document.getElementById("Two").disabled = true;
-round();
+game();
 }
 function three(){
 yours=3;
 rounds++;
 document.getElementById("Three").disabled = true;
-round();
+game();
 }
 function four(){
 yours=4;
 rounds++;
 document.getElementById("Four").disabled = true;
-round();
+game();
 }
 function five(){
 yours=5;
 rounds++;
 console.log(yours)
 document.getElementById("Five").disabled = true;
-round();
+game();
 }
 function six(){
 yours=6;
 rounds++;
 console.log(yours)
 document.getElementById("Six").disabled = true;
-round();
+game();
 }
 function seven(){
 yours=7;
 rounds++;
 document.getElementById("Seven").disabled = true;
-round();
+game();
 }
 function eight(){
 yours=8;
 rounds++;
 document.getElementById("Eight").disabled = true;
-round();
+game();
 }
 function nine(){
 yours=9;
 rounds++;
 document.getElementById("Nine").disabled = true;
-round();
+game();
 }
 function ten(){
 yours=10;
 rounds++;
 document.getElementById("Ten").disabled = true;
-round();
+game();
 }
+/////////////////
